6d295e40369a1cd55290513ec0e027ee2beba484
Khanze99 <khamidov_anvar@mail.ru>
2019-08-08 14:22:21 +0300

    #4 udpare code , add command /info

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import getpass
 import threading
 
 CONFIG = {}
-COMMANDS = {'/help': '1. </connect> - connect to ftp example \n2. </copy FROM TO>\n3. /exit ',
+USER_CONFIG = {}
+COMMANDS = {'/help': '1. </connect> - connect to ftp example \n2. </copy FROM TO>\n4. /info about server\n3. /exit ',
             '/connect': '',
             '/copy': '',
-            '/exit': ''}
+            '/exit': "Thank's good bye :)",
+            '/info': ''}
 TURN_LIST = []
 
 # host = "127.0.0.1"
@@ -14,10 +16,16 @@ TURN_LIST = []
 # password = "-"
 
 
-def connected_to_ftp(host, user, password):
-    with pysftp.Connection(host=host, username=user, password=password) as ftp:
-        print("Connected ...")
-        ftp.put('test.txt', "/home/khanze/files/test.txt")
+def get_info_about_server(**data):
+    with pysftp.Connection(**data) as ftp:
+        print("Connected to {}...".format(data['host']))
+        hostname = ftp.execute('hostname')[0].decode("ascii")
+        print(hostname)
+
+
+def connected_to_ftp(**data):
+    with pysftp.Connection(**data) as ftp:
+        print("Connected to {}...".format(data['host']))
 
 
 if __name__ == "__main__":
@@ -30,15 +38,24 @@ if __name__ == "__main__":
         command = input()
         if command == '/help':
             print(COMMANDS['/help'])
-            input("Continue?")
-
+            input("Press any button to continue")
             continue
         if command == "/connect":
-            user_config = {'host': input("Please enter your host: "),
-                           'user': input("Please enter your username: "),
-                           'password': getpass.getpass("Input your password: ")}
-            connected_to_ftp(user_config['host'], user_config['user'], user_config['password'])
+            USER_CONFIG.update({'host': input("Please enter your host: "),
+                                'username': input("Please enter your username: "),
+                                'password': getpass.getpass("Input your password: ")})
+            CONFIG.update({'host': USER_CONFIG['host'], 'username': USER_CONFIG['username']})
+            connected_to_ftp(**USER_CONFIG)
+            while True:
+                if input('Continue or exit?') == 'continue':
+                    continue
+                else:
+                    print(COMMANDS['/exit'])
+                    break
+            break
+        if command == '/info':
+            get_info_about_server(**USER_CONFIG)
         if command == '/exit':
-            print("Thank's. Good bye :)")
+            print(COMMANDS['/exit'])
             break
 
