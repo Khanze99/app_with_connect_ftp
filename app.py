98f16fe3ebf6bb32eae52439daf01f957b142356
Khanze99 <khamidov_anvar@mail.ru>
2019-08-09 12:37:35 +0300

    #6 update code add input dir to config.json

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
+from loguru import logger
 import pysftp
 import getpass
 import threading
+import json
+
 
 CHECK = False
-CONFIG = {}
-USER_CONFIG = {}
-COMMANDS = {'/help': '1. </connect> - connect to ftp example \n2. </copy FROM TO>\n4. /info about server\n3. /exit ',
-            '/connect': '',
-            '/copy': '',
-            '/exit': "Thank's good bye :)",
-            '/info': ''}
 TURN_LIST = []
 
 # host = "127.0.0.1"
@@ -17,69 +13,64 @@ TURN_LIST = []
 # password = "-"
 
 
-def copy_local_to_server(from_path, to_path):
-    with pysftp.Connection(**USER_CONFIG) as ftp:
-        print("Find file to local directory")
-        try:
+def parse_config(command):
+    try:
+        with open(command, mode='r') as config:
+            logger.debug("Read config")
+            data = json.load(config)
+    except FileNotFoundError:
+        logger.debug("I can not find the config")
+        return False
+    copy_local_to_server(**data)
+
+
+def copy_local_to_server(**data):
+    host = data['host']
+    from_path = data['from_path']
+    to_path = data['to_path']
+    try:
+        valid_open = open(from_path, mode="r")
+        valid_open.close()
+    except FileNotFoundError:
+        logger.debug("Can not find the file")
+        return False
+    try:
+        with pysftp.Connection(host=host, username=data['username'], password=data['password']) as ftp:
+            logger.debug('Connect to {}'.format(host))
             ftp.put(from_path, to_path)
-        except FileNotFoundError:
-            print("Dont such file")
-        print("Save file to server...")
-
-
-def get_info_about_server(**data):
-    with pysftp.Connection(**data) as ftp:
-        print("Connected to {}...".format(data['host']))
-        hostname = ftp.execute('hostname')[0].decode("ascii").strip()
-        CONFIG.update({'hostname': hostname})
-        for item in CONFIG:
-            print("{} - {}".format(item, CONFIG[item]))
-
-
-def connected_to_ftp(**data):
-    with pysftp.Connection(**data) as ftp:
-        print("Connected to {}...".format(data['host']))
+    except FileNotFoundError:
+        logger.debug("Can not find path to save on the server")
+        with pysftp.Connection(host=host, username=data['username'], password=data['password']) as ftp:
+            logger.debug('Connect to {}'.format(host))
+            file = from_path.split('/')[-1]
+            ftp_dir = "ftp_files"
+            pwd = ftp.pwd + '/'
+            to_save = pwd+ftp_dir+'/'+file
+            list_dirs = ftp.listdir()
+            if ftp_dir not in list_dirs:
+                ftp.mkdir(ftp_dir)
+            ftp.put(from_path, to_save)
+        logger.debug('Save to {}'.format(to_path))
 
 
 if __name__ == "__main__":
     while True:
         if CHECK is False:
             print("-----------------------------HELLO-------------------------------\n"
-                  "...........input /help for the check commands....................\n"
+                  ".................Enter the path to the config....................\n"
                   "...........--------------------------------------------..........\n"
-                  "...........input /connect for get info about ftp-server..........\n"
-                  "...........--------------------------------------------..........\n"
-                  "...........input /copy TO FROM - copied file to ftp-server..........")
+                  ".......................input /exit to exit ......................\n"
+                  "...........--------------------------------------------..........")
             CHECK = True
-        command = input('Write command :')
-        if command == '/help':
-            print(COMMANDS['/help'])
-            input("Press any button to continue")
-            continue
-        if command == "/connect":
-            USER_CONFIG.update({'host': input("Please enter your host: "),
-                                'username': input("Please enter your username: "),
-                                'password': getpass.getpass("Input your password: ")})
-            CONFIG.update({'host': USER_CONFIG['host'], 'username': USER_CONFIG['username']})
-            connected_to_ftp(**USER_CONFIG)
-            continue
-        if command == '/info':
-            if len(CONFIG) == 0:
-                print("---------------------------------------------------------\n"
-                      "You need connect to the ftp-server. Give command /connect\n"
-                      "----------------------------------------------------------")
-                continue
-            get_info_about_server(**USER_CONFIG)
-            continue
-        if command == '/copy':
-            from_path = input('Please input path to file in pc: ')
-            to_path = input('Please input path to ftp-server: ')
-            CONFIG.update({'from': from_path, 'to': to_path})
-            copy_local_to_server(from_path, to_path)
-            continue
+        command = input('Enter the path to the config or input /exit: ')
+
         if command == '/exit':
-            print(COMMANDS['/exit'])
+            logger.debug("Good bye :)")
             break
         else:
-            print("I can not find this {}".format(command))
+            logger.debug("Parse config")
+            if parse_config(command) is False:
+                continue
+
+
 
